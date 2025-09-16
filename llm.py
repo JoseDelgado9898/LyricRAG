230da75aee10e259739d6a7bc6c1cb88d8b7ea76
JoseDelgado9898 <josedv9898@gmail.com>
2025-09-15 20:09:02 -0600

    RAG functionality

---
+++ llm.py
@@ -0,0 +1,85 @@
+'''
+ToDo:
+    1. Repo
+    2. Write-up
+'''
+
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain.chat_models import init_chat_model
+from langchain_core.vectorstores import InMemoryVectorStore
+from lyrics import *
+
+os.environ.setdefault("TIKTOKEN_CACHE_DIR", r"C:\Users\delgado.jd.7\.tiktoken")
+
+llm = init_chat_model("gpt-4.1-nano", model_provider="openai")
+
+def create_documents(songs):
+    docs=[]
+    for s in songs:
+        doc = Document(
+            page_content = s['lyrics'],
+            metadata = s['metadata']
+        )
+        docs.append(doc)
+    return docs
+
+def split_documents(docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,  # chunk size (characters)
+        chunk_overlap=50,  # chunk overlap (characters)
+        add_start_index=True,  # track index in original document
+    )
+    splitted = text_splitter.split_documents(docs)
+    return splitted
+
+def create_vector_store(docs):
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    vector_store = InMemoryVectorStore(embedding=embeddings)
+    document_ids = vector_store.add_documents(documents=docs)
+    return vector_store
+
+
+def retrieve(vector_store, prompt):
+    retrieved_docs = vector_store.similarity_search_with_score(prompt)
+    for doc in retrieved_docs:
+        song_name = doc[0].metadata['name']
+        score = doc[1]
+        print(f'Song name: {song_name} , score: {score}')
+    return retrieved_docs
+
+def generate(vector_store,prompt):
+
+    context = retrieve(vector_store,prompt)
+    parsed_context = '''
+    Use the provided context to answer the user's questions.
+    Only if the answer cannot be derived from the context use your base knowledge.
+    Answer concisely
+    '''
+    for doc in context:
+        parsed_context += f'Metadata for document {doc[0].id}: {doc[0].metadata}\n Song lyrics: {doc[0].page_content}\n'
+    parsed_context+= f'User question: {prompt}'
+    response = llm.invoke(parsed_context).content
+    return response
+if __name__ == "__main__":
+
+    load_dotenv()
+    songs = generate_top_tracks_lyrics('Harry Styles')
+    docs = create_documents(songs)
+    splitted_docs = split_documents(docs)
+    prompt = 'What popular song references being perpetually stuck while running from bullets ?'
+    v_s = create_vector_store(splitted_docs)
+    response_baseline = llm.invoke(prompt).content
+    response_RAG= generate(v_s,prompt)
+    
+    print(f'Prompt:\n{prompt}')
+    print(f'Baseline response\n{response_baseline}')
+    print(f'RAG respone\n{response_RAG}')
+
+    # response_baseline = llm.invoke('What popular song talks about a specific month slipping away in a moment?').content
+    # response_RAG= generate(v_s,'What popular song talks about a specific month slipping away in a moment?')
+    # print('Baseline response\n'+response_baseline)
+    # print('RAG respone\n'+response_RAG)
---
+++ lyrics.py
@@ -0,0 +1,71 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+def generate_bearer():
+    body_data = {
+        "grant_type":"client_credentials",
+        "client_id":os.getenv('SPOTIFY_CLIENT_ID'),
+        "client_secret":os.getenv('SPOTIFY_CLIENT_SECRET')
+    }
+    response = requests.post(
+        "https://accounts.spotify.com/api/token",
+        data = body_data,
+        verify=False
+     )
+    access_token = response.json()['access_token']
+    return access_token
+
+def get_artist_id(token, artist_name):
+    headers = {"Authorization":f"Bearer {token}"}
+    response = requests.get(f'https://api.spotify.com/v1/search?q=artist:{artist_name}&type=artist', headers=headers).json()
+    return response['artists']['items'][0]['id'] #retrieve ID
+
+def get_top_tracks(token,artist_id):
+    headers = {"Authorization":f"Bearer {token}"}
+    response = requests.get(f'https://api.spotify.com/v1/artists/{artist_id}/top-tracks', headers=headers).json()
+    tracks = []
+    for track in response['tracks']:
+        tracks.append(
+            {
+                'name':track['name'],
+                'id':track['id'],
+                'artist':track['artists'][0]['name']
+            }
+        )
+    return tracks
+
+def get_lyrics(artist_name, song_name):
+    response = requests.get(f'https://api.lyrics.ovh/v1/{artist_name}/{song_name}')
+    status_code = response.status_code
+    if(status_code==404):
+        return 'Song not found'
+    elif status_code==200:
+        return response.json()['lyrics']
+    
+def generate_top_tracks_lyrics(artist_name):
+    token = generate_bearer()
+    artist_id = get_artist_id(token,artist_name)
+    top_tracks = get_top_tracks(token,artist_id)
+    lyrics = []
+    for track in top_tracks:
+        lyrics.append(
+            {
+            'metadata':track,
+            'lyrics':get_lyrics(artist_name,track['name'])
+            }
+        )
+
+    return lyrics
+
+if __name__ == "__main__":
+    load_dotenv()
+    token = generate_bearer()
+    artist_id = get_artist_id('Taylor Swift')
+    top_tracks = get_top_tracks(token,'06HL4z0CvFAxyc27GXpf02')
+    #print(generate_lyrics(top_tracks))
+    # artists = [('Taylor Swift','06HL4z0CvFAxyc27GXpf02'),('Gracie Abrams','4tuJ0bMpJh08umKkEXKUI5')]
+    # for artist in artists:
+    # top_tracks = get_top_tracks(token,)
+    # lyrics_db = generate_lyrics_list(top_tracks,'Taylor Swift')
+    # print(len(lyrics_db))
